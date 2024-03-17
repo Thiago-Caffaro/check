3a0f2851a0f0dfb05f66fcd9268d068989c66baf
Thiago <thiagocaffaro223@gmail.com>
2024-03-17 16:47:38 -0300

    projeto iniciado

---
+++ script.py
@@ -0,0 +1,8 @@
+import os
+
+if os.path.exists('C:/Users/Thiago/Downloads/a/CP2077_photorealistic_2.0.ini'):
+    print('O arquivo existe!')
+else:
+    print('O arquivo não foi encontrado.')
+def checkForProgram(dir):
+

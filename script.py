3974e454be00c4f149a7e50f78c15f0f006db6d5
Thiago <thiagocaffaro223@gmail.com>
2024-03-17 17:50:53 -0300

    Projeto base pronto, junto com comentários, agora sendo posível verificar um caminho dado e seus filhos para procurar um nome específico

--- script.py
+++ script.py
@@ -1,8 +1,39 @@
 import os
 
-if os.path.exists('C:/Users/Thiago/Downloads/a/CP2077_photorealistic_2.0.ini'):
-    print('O arquivo existe!')
-else:
-    print('O arquivo não foi encontrado.')
-def checkForProgram(dir):
 
+def checkforprogram(directory, targetp):
+
+    # Função que checa em um item dado, se X string existe nela
+    # Também recebe o caminho para quando achar, printar uma mensagem personalizada
+    def check(p_targetp, p_item, p_path):
+        if p_targetp in p_item:
+            print(f"Achouu!\n{p_targetp} no caminho {p_path}")
+
+    try:
+        dirlist = os.listdir(directory)
+        # Cria uma lista baseada nos elementos encontrados em "dir"
+
+        if os.path.exists(directory):
+            for item in dirlist:
+                itemdir = os.path.join(directory, item)
+                # Caminho do item atual, criado ao juntar o diretório passado pelo parâmetro com o item atual
+
+                # Se for um diretório checa e faz uma chamada recursiva para verificar o mesmo
+                if os.path.isdir(itemdir):
+                    check(targetp, item, itemdir)
+                    checkforprogram(itemdir, targetp)
+
+                # Se for um arquivo apenas checa
+                elif os.path.isfile(itemdir):
+                    check(targetp, item, itemdir)
+
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')
+
+
+targetP = str(input("Digite o programa alvo: "))
+caminho1 = "C:/Program Files"
+caminho2 = "C:/Program Files (x86)"
+
+checkforprogram(caminho1, targetP)
+checkforprogram(caminho2, targetP)
